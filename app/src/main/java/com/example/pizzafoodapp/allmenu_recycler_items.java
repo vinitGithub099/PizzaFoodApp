888f7e593f1bf3a3eca73568bff995ec3f540765
Twinshu <twinshup@gmail.com>
2022-11-19 17:07:37 +0530

    xml added

---
+++ app/src/main/java/com/example/pizzafoodapp/allmenu_recycler_items.java
@@ -0,0 +1,14 @@
+package com.example.pizzafoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class allmenu_recycler_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.allmenu_recycler_items);
+    }
+}
